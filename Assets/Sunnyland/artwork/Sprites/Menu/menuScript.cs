b2b601a5169d1ef7d1d56727cff1846791c1f276
joao-ranieri <joao.s.raniei@gmail.com>
2019-06-04 23:46:36 -0300

    Criação da animação do menu.

---
+++ Assets/Sunnyland/artwork/Sprites/Menu/menuScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuScript : MonoBehaviour{
+
+    private float scrollSpeed = 0.1f;
+    private MeshRenderer meshRender;
+    private Vector2 savedOffset;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    private void Awake(){
+        meshRender = GetComponent<MeshRenderer>();
+        savedOffset = meshRender.sharedMaterial.GetTextureOffset("_MainTex");
+    }
+
+    // Update is called once per frame
+    void Update(){
+        float x = Time.time * scrollSpeed;
+        Vector2 offset = new Vector2(x, 0);
+        meshRender.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+
+    private void OnDisable(){
+        meshRender.sharedMaterial.SetTextureOffset("_MainTex", savedOffset);
+    }
+}
